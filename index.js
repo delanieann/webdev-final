2a70c3cedcb5f03c0deb418c6f21272888d7c8e5
Delanie <degraham@pdx.edu>
2025-03-13 21:21:39 -0700

    Added form, projects, API

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
+
+function getRandom() {
+    return Math.floor(Math.random() * (84000)) + 1;
+}
+
+
 function makeAboutMe() {
     let aboutMe = document.querySelector("#about");
 
@@ -15,6 +22,38 @@ function makeAboutMe() {
     card.appendChild(text);
 
     aboutMe.appendChild(card);
-}   
+};
 
-makeAboutMe();
+const fetchData = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        createArt(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        console.log("Finally.");
+      });
+  };
+
+  const createArt = (data) => {
+    let art = document.querySelector("#art");
+    let div = document.createElement("div");
+    div.classList.add("card", "border-0", "rounded", "rounded-4");
+
+    div.innerHTML = `
+    <img src="${data.primaryImage}" class="img-card-top" alt="${data.objectName}" style="max-width: 540px;">
+        <div class="card-body">
+            <p class="card-text">${data.title} by ${data.artistDisplayName}, ${data.objectDate}</p>
+        </div>
+    `
+    art.appendChild(div);
+  }
+
+  makeAboutMe();
+
+  let randomURL = url + getRandom();
+  fetchData(randomURL);
